3b6db9e1b24dc6d136b7a06b57d5c851a3363df7
Jorge Moya <jorge.moya@bigcommerce.com>
2025-04-08 15:14:02 -0500

    fix: remove variant

--- core/app/[locale]/(default)/product/[slug]/page-data.ts
+++ core/app/[locale]/(default)/product/[slug]/page-data.ts
@@ -104,13 +104,6 @@ export const ProductFormFragment = graphql(
   `
     fragment ProductFormFragment on Product {
       entityId
-      variants {
-        edges {
-          node {
-            entityId
-          }
-        }
-      }
       productOptions(first: 50) {
         edges {
           node {
